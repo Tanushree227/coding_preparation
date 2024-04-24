fd47d2adb6f3eadc76e5180bbd111c34850c511d
Tanushree227 <tanushreedas813@gmail.com>
2024-04-24 23:38:39 +0530

    adding landing page

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import Week1 from "./pages/Week1";
 import Week3 from "./pages/Week3";
 import Week4 from "./pages/Week4";
 import Week2 from "./pages/Week2";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <Routes>
+      <Route path="/" element={<Home />} />
       <Route path="/week1" element={<Week1 />} />
       <Route path="/week2" element={<Week2 />} />
       <Route path="/week3" element={<Week3 />} />
