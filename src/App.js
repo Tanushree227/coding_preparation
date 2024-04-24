026df4a00284fab17baff26f238aa826e9496358
Tanushree227 <tanushreedas813@gmail.com>
2024-04-24 23:36:01 +0530

    adding routes for weekly basis

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import { Route, Routes } from "react-router-dom";
+import Week1 from "./pages/Week1";
+import Week3 from "./pages/Week3";
+import Week4 from "./pages/Week4";
+import Week2 from "./pages/Week2";
+
 function App() {
   return (
-    <div>
-      <h1>Coding Interview Preparation</h1>
-    </div>
+    <Routes>
+      <Route path="/week1" element={<Week1 />} />
+      <Route path="/week2" element={<Week2 />} />
+      <Route path="/week3" element={<Week3 />} />
+      <Route path="/week4" element={<Week4 />} />
+    </Routes>
   );
 }
 
