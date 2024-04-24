cb56828e2f907898dca0cec94708edd0ba4e66d5
Tanushree227 <tanushreedas813@gmail.com>
2024-04-24 23:45:46 +0530

    installed tailwind css

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div>Coding Interview Preparation</div>
+    <div>
+        <h1>Coding Interview Preparation</h1>
+    </div>
   )
 }
