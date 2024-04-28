dc7bd839dd808a097b2d4f5d770fa7c043545eca
Tanushree227 <tanushreedas813@gmail.com>
2024-04-28 12:17:27 +0530

    done with layout and navigation

---
+++ src/components/layout/MainNavigation.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import classes from './MainNavigation.module.css';
+
+function MainNavigation()
+{
+
+    return(
+        <header className={classes.header}>
+            <div className={classes.logo}>EchoHub</div>
+            <nav>
+                <ul>
+                    <li><Link to="/" exact>Home</Link></li>
+                    <li><Link to="/week1">Module 1</Link></li>
+                    <li><Link to="/week2">Module 2</Link></li>
+                    <li><Link to="/week3">Module 3</Link></li>
+                    <li><Link to="/week4">Module 4</Link></li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default MainNavigation;
