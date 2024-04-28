002989d1993457a4e4616f400c7fec97149a180f
Tanushree227 <tanushreedas813@gmail.com>
2024-04-28 22:08:35 +0530

    added content for home component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
+import Card from "../components/ui/Card";
 
 export default function Home() {
   return (
-    <div>
+    <Card>
+      <div className="p-5 text-center">
         <h1>Coding Interview Preparation</h1>
-    </div>
-  )
+        <p>
+          Coding interviews are a form of technical interviews used to access a
+          potential software engineer candidate's competencies through
+          presenting them with programming problems.
+        </p>
+        <p>
+          Typically, coding interviews have a focus on data structures and
+          algorithms, while other technical rounds may encompass system design.
+        </p>
+        <br />
+        <p>
+          A coding interview round is typically 30 - 45 minutes. You will be
+          given a technical question (or questions) by the interviewer, and will
+          be expected to write code in a real-time collaborative editor such as
+          CodePen or CoderPad (phone screen / virtual onsite) or on a whiteboard
+          (onsite) to solve the problem within 30 to 45 minutes.
+        </p>
+      </div>
+    </Card>
+  );
 }
