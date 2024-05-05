58353b78b69db8a9a7b162b9d35538e03883a913
Tanushree227 <tanushreedas813@gmail.com>
2024-05-05 23:46:12 +0530

    starting writing content for week1

--- src/pages/Week1.js
+++ src/pages/Week1.js
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function Week1() {
   return (
-    <div>Week1</div>
+    <div className='card shadow-xl bg-slate-200'>
+      <h1 className='text-center'>Week 1</h1>
+    </div>
   )
 }
