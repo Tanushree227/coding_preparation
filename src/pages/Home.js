7cad8eb6fe8cd4735b5a3860baa5c6d15e040626
Tanushree227 <tanushreedas813@gmail.com>
2024-05-05 23:43:06 +0530

    mentioned types of interview

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,7 @@ import Card from "../components/ui/Card";
 export default function Home() {
   return (
     <Card>
-      <div className="p-5 text-center">
+      <div className="m-3 p-5 text-center">
         <h1>Coding Interview Preparation</h1>
         <p>
           Coding interviews are a form of technical interviews used to access a
@@ -24,8 +24,15 @@ export default function Home() {
           (onsite) to solve the problem within 30 to 45 minutes.
         </p>
       </div>
-      <div>
+      <div className="m-3 p-5 text-center">
         <h1>Types of Interview: </h1>
+        <ul>
+          <li>Online Assessment</li>
+          <li>Online Interview</li>
+          <li>Screening Interview</li>
+          <li>Coding Assessment</li>
+          <li>Follow up Interview</li>
+        </ul>
       </div>
     </Card>
   );
